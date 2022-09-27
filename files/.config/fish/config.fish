6099c3859f2537ef11b42df66a12883f056122de
Andrew Kampjes <a.kampjes@gmail.com>
2022-09-27 22:38:25 +1300

    alias gpf = git push --force-with-lease

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -3,6 +3,7 @@ alias gdc='git diff --cached'
 alias be='bundle exec'
 alias gau 'git add -u'
 alias gg 'git grep'
+alias gpf 'git push --force-with-lease'
 alias vim 'nvim'
 alias vimdiff='nvim -d'
 
